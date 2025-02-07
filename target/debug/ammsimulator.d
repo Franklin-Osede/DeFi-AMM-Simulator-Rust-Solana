c985b10a76d5f97b9de5a2c9e036d6b9d8a36f2c
Franklin <71532004+fran1014@users.noreply.github.com>
2025-02-07 22:24:35 +0100

    compile proyect and added folder structure

---
+++ target/debug/ammsimulator.d
@@ -0,0 +1,1 @@
+C:\Users\frank\OneDrive\Documents\GitHub\DeFi-AMM-Simulator-Rust-Solana\target\debug\ammsimulator.exe: C:\Users\frank\OneDrive\Documents\GitHub\DeFi-AMM-Simulator-Rust-Solana\src\main.rs
---
+++ target/debug/deps/ammsimulator.d
@@ -0,0 +1,5 @@
+C:\Users\frank\OneDrive\Documents\GitHub\DeFi-AMM-Simulator-Rust-Solana\target\debug\deps\ammsimulator.exe: src\main.rs
+
+C:\Users\frank\OneDrive\Documents\GitHub\DeFi-AMM-Simulator-Rust-Solana\target\debug\deps\ammsimulator.d: src\main.rs
+
+src\main.rs:
