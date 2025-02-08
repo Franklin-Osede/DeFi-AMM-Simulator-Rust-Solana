69d1eb7660d055644df29134a0004ad0e0e24fab
Franklin <71532004+fran1014@users.noreply.github.com>
2025-02-08 09:46:37 +0100

    install solana cli

---
+++ target/debug/deps/ammsimulator-f48790012f7e1189.d
@@ -0,0 +1,5 @@
+C:\Users\frank\OneDrive\Documents\GitHub\DeFi-AMM-Simulator-Rust-Solana\target\debug\deps\libammsimulator-f48790012f7e1189.rmeta: src\main.rs
+
+C:\Users\frank\OneDrive\Documents\GitHub\DeFi-AMM-Simulator-Rust-Solana\target\debug\deps\ammsimulator-f48790012f7e1189.d: src\main.rs
+
+src\main.rs:
